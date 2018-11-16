9caf84820cab2a2aa1d142263b31776ae559dd42
Shawn Bickel <sbickel201@gmail.com>
2018-11-16 13:14:20 -0500

    test endpoint

--- src/main/java/com/topshelf/controllers/ChefController.java
+++ src/main/java/com/topshelf/controllers/ChefController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,24 +28,29 @@ public class ChefController {
 		this.chefService = chefService;
 	}
 	
-	@PostMapping(value="/login", consumes=MediaType.APPLICATION_JSON_VALUE, produces=MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<Boolean> login(@RequestBody String[] chefCredentials) {
-		Chef loggedInChef = chefService.loginChef(chefCredentials[0], chefCredentials[1]);
-		if (loggedInChef == null) {
-			return new ResponseEntity<Boolean>(false, HttpStatus.NOT_FOUND);
-		}
-		
-		return new ResponseEntity<Boolean>(true, HttpStatus.OK);
+	@GetMapping(value="/login", produces=MediaType.TEXT_PLAIN_VALUE)
+	public String test() {
+		return "Our ec2 is working!";
 	}
 	
-	@PostMapping(value="/register", consumes=MediaType.APPLICATION_JSON_VALUE, produces=MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<Chef> addNewChef(@RequestBody Chef newChef) {
-		Chef registeredChef = chefService.addChef(newChef);
-		
-		if (registeredChef == null) {
-			return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
-		}
-		return new ResponseEntity<Chef>(registeredChef, HttpStatus.CREATED);
-	}
+//	@GetMapping(value="/login", consumes=MediaType.APPLICATION_JSON_VALUE, produces=MediaType.APPLICATION_JSON_VALUE)
+//	public ResponseEntity<Boolean> login(@RequestBody String[] chefCredentials) {
+//		Chef loggedInChef = chefService.loginChef(chefCredentials[0], chefCredentials[1]);
+//		if (loggedInChef == null) {
+//			return new ResponseEntity<Boolean>(false, HttpStatus.NOT_FOUND);
+//		}
+//		
+//		return new ResponseEntity<Boolean>(true, HttpStatus.OK);
+//	}
+//	
+//	@GetMapping(value="/register", consumes=MediaType.APPLICATION_JSON_VALUE, produces=MediaType.APPLICATION_JSON_VALUE)
+//	public ResponseEntity<Chef> addNewChef(@RequestBody Chef newChef) {
+//		Chef registeredChef = chefService.addChef(newChef);
+//		
+//		if (registeredChef == null) {
+//			return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
+//		}
+//		return new ResponseEntity<Chef>(registeredChef, HttpStatus.CREATED);
+//	}
 	
 }
